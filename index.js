1446d4861f907576d5fe8a2d04063bbcca1e11a9
Anton Sokolovsky <sokolovskii.anton94@gmail.com>
2019-11-27 05:37:51 +0300

    Добавлена боковая панель

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import View from './src/view.js'
 
 const root = document.querySelector('#root');
 const game = new Game();
-const view = new View(root, 320, 640, 20, 10);
+const view = new View(root, 480, 640, 20, 10);
 
 window.game = game;
 window.view = view;
@@ -31,3 +31,4 @@ document.addEventListener('keydown', event => {
             
     }
 })
+
--- src/view.js
+++ src/view.js
@@ -1,4 +1,13 @@
 export default class View {
+    static colors = {
+        '1': 'cyan',
+        '2': 'blue',
+        '3': 'orange',
+        '4': 'yellow',
+        '5': 'green',
+        '6': 'purple',
+        '7': 'red'
+    };
     constructor(element, width, height, rows, columns) {
         this.element = element;
         this.width = width;
@@ -9,22 +18,68 @@ export default class View {
         this.canvas.height = this.height;
         this.context = this.canvas.getContext('2d');
 
-        this.blockWidth = this.width / columns;
-        this.blockHeigth = this.height / rows;
+        this.playFieledBorderWidth = 4;
+        this.playfieldX = this.playFieledBorderWidth;
+        this.playfieldY = this.playFieledBorderWidth;
+        this.playfieldWidth = this.width * 2 / 3;
+        this.playfieldHeight = this.height;
+        this.playfieldInnerWidth = this.playfieldWidth - this.playFieledBorderWidth * 2;
+        this.playfieldInnerHeigth = this.playfieldHeight - this.playFieledBorderWidth * 2;
+
+
+        this.blockWidth = this.playfieldInnerWidth / columns;
+        this.blockHeigth = this.playfieldInnerHeigth / rows;
+
+        this.panelX = this.playfieldWidth + 10;
+        this.panelY = 0;
+        this.panelWidth = this.width / 3;
+        this.panelHeigth = this.height;
 
         this.element.appendChild(this.canvas);
     }
 
-    render({ playfield }) {
+    renderMainScreen(state) {
         this.clearScreen();
-        this.renderPlayField(playfield)
+        this.renderPlayField(state);
+        this.renderPanel(state);
+    }
+
+    renderStartScreen() {
+        this.context.fillStyle = 'white';
+        this.context.font = '18px "Time New Roman"';
+        this.context.textAlign = 'center';
+        this.context.textBaseline = 'middle';
+        this.context.fillText('Press ENTER to start', this.width / 2, this.height / 2)
+    }
+
+    renderPauseScreen() {
+        this.context.fillStyle = 'rgba(0,0,0,0.75)'
+        this.context.fillRect(0, 0, this.width, this.height);
+
+        this.context.fillStyle = 'white';
+        this.context.font = '18px "Time New Roman"';
+        this.context.textAlign = 'center';
+        this.context.textBaseline = 'middle';
+        this.context.fillText('Press ENTER to start', this.width / 2, this.height / 2);
+    }
+
+    renderEndScreen({ score }) {
+        this.clearScreen();
+
+        this.context.fillStyle = 'white';
+        this.context.font = '18px "Time New Roman"';
+        this.context.textAlign = 'center';
+        this.context.textBaseline = 'middle';
+        this.context.fillText('Game Over', this.width / 2, this.height / 2 - 48);
+        this.context.fillText(`Score: ${score}`, this.width / 2, this.height / 2);
+        this.context.fillText('Press F5 to continue', this.width / 2, this.height / 2 + 48);
     }
 
     clearScreen() {
         this.context.clearRect(0, 0, this.width, this.height)
     }
 
-    renderPlayField(playfield) {
+    renderPlayField({ playfield }) {
         for (let y = 0; y < playfield.length; y++) {
             const line = playfield[y];
 
@@ -32,7 +87,32 @@ export default class View {
                 const block = line[x];
 
                 if (block) {
-                    this.renderBlock(x * this.blockWidth, y * this.blockHeigth, this.blockWidth, this.blockHeigth, 'red')
+                    this.renderBlock(this.playfieldX + (x * this.blockWidth),this.playfieldY + (y * this.blockHeigth), this.blockWidth, this.blockHeigth, View.colors[block])
+                }
+            }
+        }
+        this.context.strokeStyle = 'white';
+        this.context.lineWidth = this.playFieledBorderWidth;
+        this.context.strokeRect(0, 0, this.playfieldWidth, this.playfieldHeight);
+    }
+
+    renderPanel({ level, score, lines, nextPiece }) {
+        this.context.textAlign = 'start';
+        this.context.textBaseline = 'top';
+        this.context.fillStyle = 'white';
+        this.context.font = '18px "Time New Roman"';
+
+        this.context.fillText(`Score: ${score}`, this.panelX, this.panelY + 0);
+        this.context.fillText(`Lines: ${lines}`, this.panelX, this.panelY + 24);
+        this.context.fillText(`Level: ${level}`, this.panelX, this.panelY + 48);
+        this.context.fillText('Next', this.panelX, this.panelY + 96);
+
+        for (let y = 0; y < nextPiece.blocks.length; y++) {
+            for (let x = 0; x < nextPiece.blocks[y].length; x++) { 
+                const block = nextPiece.blocks[y][x];
+
+                if (block) {
+                    this.renderBlock(this.panelX + (x * this.blockWidth * 0.5), this.panelY + 120 +(y * this.blockHeigth * 0.5), this.blockWidth * 0.5, this.blockHeigth * 0.5, View.colors[block])
                 }
             }
         }
