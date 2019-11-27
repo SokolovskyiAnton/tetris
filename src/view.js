b9820034099df7e12aecdefa22c92f6e3ebaabce
Anton Sokolovsky <sokolovskii.anton94@gmail.com>
2019-11-27 06:17:45 +0300

    test

--- src/view.js
+++ src/view.js
@@ -51,6 +51,7 @@ export default class View {
         this.context.textBaseline = 'middle';
         this.context.fillText('Press ENTER to start', this.width / 2, this.height / 2)
         this.context.fillText('PC version', this.width / 2, this.height / 2 + 48);
+        this.context.fillText('PC version', this.width / 2, this.height / 2 + 96);
     }
 
     renderPauseScreen() {
